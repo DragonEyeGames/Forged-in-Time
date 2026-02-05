e178cf123555906146f41852f870dcdafce30bec
Taxalottle Syndicate <bthole41@gmail.com>
2026-02-05 18:26:03 -0500

    Tile placement but corner clipping
    I made it so we can actually place blocks on the map but the corners get cut by the guy. Working to fix that.

---
+++ Base.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class Base : Sprite2D
+{
+	private RayCast2D ray;
+	private NavigationAgent2D navAgent;
+	private Node2D target;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		navAgent = GetNode<NavigationAgent2D>("NavAgent");
+		target = GetNode<Node2D>("../Icon2");
+		ray = GetNode<RayCast2D>("RayCast2D");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		CheckValid();
+	}
+	
+	public void CheckValid(){
+		navAgent.TargetPosition=target.GlobalPosition;
+		GD.Print(navAgent.IsTargetReachable());
+		GD.Print(Reachable());
+	}
+	
+	public bool Reachable(){
+		Vector2[] path = navAgent.GetCurrentNavigationPath();
+		for (int i = 0; i < path.Length-1; i++)
+		{
+			Vector2 basePoint = path[i];
+			Vector2 targetPoint = path[i+1];
+			
+		}
+		return true;
+	}
+}
---
+++ Placement.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public partial class Placement : TileMapLayer
+{
+	private Vector2I hoveredCell = new Vector2I(0, 0);
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector2 mouseWorldPos = GetGlobalMousePosition();
+		Vector2I cell = LocalToMap(mouseWorldPos);
+		Vector2I hoverCoords = new Vector2I(0, 1);
+		Vector2I placedCoords = new Vector2I(1, 1);
+		if(cell!=hoveredCell && GetCellSourceId(cell) != -1){
+			Vector2I atlasCoords = GetCellAtlasCoords(cell);
+			GD.Print(atlasCoords);
+			if(atlasCoords!=placedCoords){
+				Vector2I baseCoords = new Vector2I(0, 0);
+				Vector2I lastCoords = GetCellAtlasCoords(hoveredCell);
+				if(lastCoords!=placedCoords){
+					SetCell(hoveredCell, 0, baseCoords);
+				}
+				SetCell(cell, 0, hoverCoords);
+				hoveredCell=cell;
+				if(Input.IsActionPressed("Click")){
+					SetCell(cell, 0, placedCoords);
+				}
+			} else if (hoveredCell!=cell) {
+				Vector2I baseCoords = new Vector2I(0, 0);
+				SetCell(hoveredCell, 0, baseCoords);
+			}
+		}
+		if(Input.IsActionPressed("Click")){
+			SetCell(cell, 0, placedCoords);
+		}
+	}
+	
+		
+}
