f9d139e760dd63f00b0f846e1fa8c77b86ee5454
Taxalottle Syndicate <bthole41@gmail.com>
2026-02-13 09:14:54 -0500

    Proper Releasing
    The troops can now be properly released. They also have a slight delay to prevent clumping.

--- Scripts/Base.cs
+++ Scripts/Base.cs
@@ -9,26 +9,56 @@ public partial class Base : Sprite2D
 	[Export]
 	public PackedScene troop;
 	[Export] public int maxTroops=15;
+	private bool releaseTime=false;
 	public enum Troops{
 		Melee,
 		Ranged,
 		Brute,
 		Healer
 	}
+	public bool releasing=false;
 	public List<Troops> reserveTroops = new List<Troops>();
 
 	public override void _Ready(){
 		GameManager.player1Base=this;
 	}
 
-	public void spawnTroop(){
-		Troop newTroop = troop.Instantiate() as Troop;
-		GetParent().AddChild(newTroop);
-		newTroop.GlobalPosition=GlobalPosition;
-		newTroop.target=target;
+	public void spawnTroop(Troops troopType){
+		if(troopType==Troops.Melee){
+			Troop newTroop = troop.Instantiate() as Troop;
+			GetParent().AddChild(newTroop);
+			newTroop.GlobalPosition=GlobalPosition;
+			newTroop.target=target;
+		}
+		
 	}
 	
 	public override void _Process(double delta){
+		if(releasing && reserveTroops.Count>=1 && !releaseTime){
+			releaseTroop();
+		} else if (reserveTroops.Count==0){
+			releasing=false;
+		}
 		GetNode<RichTextLabel>("HUD2/Storage/Troops").Text=reserveTroops.Count.ToString() + "/" + maxTroops.ToString() + " Troops";
+	}
+	
+	public void toggle(){
+		GetNode<ColorRect>("HUD2").Visible=!GetNode<ColorRect>("HUD2").Visible;
+	}
+	
+	public void release(){
+		releasing=true;
+		if(releasing && reserveTroops.Count>=1){
+			spawnTroop(reserveTroops[0]);
+			reserveTroops.RemoveAt(0);
+		}
+	}
+	
+	private async void releaseTroop(){
+		releaseTime=true;
+		await ToSignal(GetTree().CreateTimer(0.15f), Timer.SignalName.Timeout);
+		spawnTroop(reserveTroops[0]);
+		reserveTroops.RemoveAt(0);
+		releaseTime=false;
 	}
 }
