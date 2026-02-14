e834f0d0b9c6fe71f03fc893560798add087d57f
Taxalottle Syndicate <bthole41@gmail.com>
2026-02-14 14:02:50 -0500

    Improved HUD Controls
    I made it so the HUD could also be controlled by the D-Pad. I also improved how and when the dropdown can be opened. Time to make it so we can place  the towers with controller!

--- Scripts/Controller.cs
+++ Scripts/Controller.cs
@@ -9,11 +9,16 @@ public partial class Controller : Button
 	[Export] Controller right;
 	[Export] bool selected;
 	[Export] Hud hud;
+	[Export] ColorRect holder;
+	public Vector2 baseSize;
+	public Vector2 increasedSize;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		baseSize=Scale;
+		increasedSize=Scale*=new Vector2(1.1f, 1.1f);
 		if(selected){
-			Scale*=new Vector2(1.1f, 1.1f);
+			Scale=increasedSize;
 		} else {
 			Modulate = Colors.Gray;
 		}
@@ -23,21 +28,21 @@ public partial class Controller : Button
 	public override void _Process(double delta)
 	{
 		if(selected){
-			if(hud.input=="Up" && up!=null){
+			if(hud.input=="Up" && up!=null && holder.Visible){
 				hud.input="";
 				selected=false;
 				up.selected=true;
-				up.Scale*=new Vector2(1.1f, 1.1f);
-				Scale/=new Vector2(1.1f, 1.1f);
+				up.Scale=up.increasedSize;
+				Scale=baseSize;
 				Modulate = Colors.Gray;
 				up.Modulate = Colors.White;
 			}
-			if(hud.input=="Down" && down!=null){
+			if(hud.input=="Down" && down!=null && holder.Visible){
 				hud.input="";
 				selected=false;
 				down.selected=true;
-				down.Scale*=new Vector2(1.1f, 1.1f);
-				Scale/=new Vector2(1.1f, 1.1f);
+				down.Scale=down.increasedSize;
+				Scale=baseSize;
 				Modulate = Colors.Gray;
 				down.Modulate = Colors.White;
 			}
