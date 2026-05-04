2f2573549a176c3e56ef6ecc5aee10e5062b2993
Taxalottle Syndicate <133176852+DragonEyeGames@users.noreply.github.com>
2026-05-04 12:23:26 -0400

    tower upgrades persistent
    you can time advance and it makes the towers auto upgrade! Only the turret though. The others need some work.

--- Towers/Scripts/Tower.cs
+++ Towers/Scripts/Tower.cs
@@ -22,7 +22,18 @@ public abstract partial class Tower : Node2D
 				GetNode<Area2D>("Checker/Player2").QueueFree();
 			}
 		}
+		if(Player1){
+			upgrade(Player1Manager.upgradeLevel);
+		}
+		GetNode<SignalBus>("/root/SignalBus").Connect(
+			SignalBus.SignalName.TimeAdvance,
+			new Callable(this, nameof(OnTimeAdvance))
+		);
 	}
+	
+	public virtual void OnTimeAdvance(bool upgradePlayer, int level){}
+	
+	public virtual void upgrade(int level){}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
