81eb51b43605dc52d710ce9496113dbfe6f9ed3d
Splooble <155033535+MrWaffle09@users.noreply.github.com>
2026-05-06 12:22:27 -0400

    beanzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz

--- Miner.cs
+++ Miner.cs
@@ -3,47 +3,62 @@ using System;
 
 public partial class Miner : TargetBase
 {
-	public override int health { get; set; } = 200;
-	public override int maxHealth { get; set; } = 200;
+	public override int health { get; set; } = 100;
+	public override int maxHealth { get; set; } = 100;
 	public override bool isBase { get; set; } = false;
 	public bool playerKilled;
 	private int playerOwned = 0;
 	[Export] public int moneyGenerated = 100;
 
 
-	public void money()
+	public void money(int moneyAmount)
 	{
+		if (playerOwned == 1) ;
+		{
+			
+		}
 	}
 	public async override void Die()
 	{
+		GD.Print(playerOwned);
 		if (playerKilled)
 		{
-			GameManager.player1Target=GameManager.player1DefaultTarget;
-			GD.Print("aplles");
-			GetNode<Area2D>("Player-2").SetCollisionLayerValue(6, true);
-			GetNode<Area2D>("Player-None").SetCollisionLayerValue(6, true);
-			await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
-			GetNode<TerritoryChecker>("../Territory").recalculate();
-			GetNode<Area2D>("Player-2").SetCollisionLayerValue(6, false);
-			GetNode<Area2D>("Player-None").SetCollisionLayerValue(6, false);
-			GetNode<Area2D>("Player-1").SetCollisionLayerValue(4, true);
-			await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
-			GetNode<TerritoryChecker>("../Territory").recalculate();
-
+			if (playerOwned != 1)
+			{
+				playerOwned = 1;
+				GameManager.player1Target = GameManager.player1DefaultTarget;
+				GetNode<Area2D>("Player-2").SetCollisionLayerValue(5, false);
+				GetNode<Area2D>("Player-2").SetCollisionLayerValue(6, true);
+				GetNode<Area2D>("Player-None").SetCollisionLayerValue(12, false);
+				GetNode<Area2D>("Player-None").SetCollisionLayerValue(6, true);
+				await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+				GetNode<TerritoryChecker>("../Territory").recalculate();
+				GetNode<Area2D>("Player-2").SetCollisionLayerValue(6, false);
+				GetNode<Area2D>("Player-None").SetCollisionLayerValue(6, false);
+				GetNode<Area2D>("Player-1").SetCollisionLayerValue(4, true);
+				await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+				GetNode<TerritoryChecker>("../Territory").recalculate();
+				
+			}
 
 		}
 		else if (playerKilled == false)
 		{
-			GameManager.player2Target=GameManager.player2DefaultTarget;
-			GetNode<Area2D>("Player-1").SetCollisionLayerValue(6, true);
-			GetNode<Area2D>("Player-None").SetCollisionLayerValue(6, true);
-			GetNode<TerritoryChecker>("../Territory").recalculate();
-			GetNode<Area2D>("Player-1").SetCollisionLayerValue(6, false);
-			GetNode<Area2D>("Player-None").SetCollisionLayerValue(6, false);
-			GetNode<Area2D>("Player-2").SetCollisionLayerValue(5, true);
-			GetNode<TerritoryChecker>("../Territory").recalculate();
-
-
+			if (playerOwned != 2)
+			{
+				GameManager.player2Target = GameManager.player2DefaultTarget;
+				GetNode<Area2D>("Player-1").SetCollisionLayerValue(4, false);
+				GetNode<Area2D>("Player-1").SetCollisionLayerValue(6, true);
+				GetNode<Area2D>("Player-None").SetCollisionLayerValue(12, false);
+				GetNode<Area2D>("Player-None").SetCollisionLayerValue(6, true);
+				GetNode<TerritoryChecker>("../Territory").recalculate();
+				GetNode<Area2D>("Player-1").SetCollisionLayerValue(6, false);
+				GetNode<Area2D>("Player-None").SetCollisionLayerValue(6, false);
+				GetNode<Area2D>("Player-2").SetCollisionLayerValue(5, true);
+				GetNode<TerritoryChecker>("../Territory").recalculate();
+				playerOwned = 2;
+			}
+			health = maxHealth;
 
 		}
 
